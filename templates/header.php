48f1d750c99876f58dc198090143cae686c485a2
José SAYAGO <jose.sayago@laelite.info>
2015-03-06 01:09:34 -0430

    Menu Superior
    Añadido menu superior en su etapa inicial.

--- templates/header.php
+++ templates/header.php
@@ -22,3 +22,29 @@
     <script>document.createElement( "picture" );</script>
 </head>
 <body <?php body_class(); ?>>
+    <!-- \Menu -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion">
+            <span class="sr-only">Mostrar Menú</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navegacion">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#n">Nosotros &nbsp;&nbsp;&nbsp;/</a></li>
+            <li><a href="#x">Certificación &nbsp;&nbsp;&nbsp;/</a></li>
+            <li><a href="#c">Caras Felices &nbsp;&nbsp;&nbsp;/</a></li>
+            <li><a href="#o">Contacto</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <!-- /Menu -->
